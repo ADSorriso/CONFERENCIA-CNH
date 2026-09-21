edaa79e6911af0a9444372bd4c4167114942c6a0
ADSorriso <ricaels55@gmail.com>
2026-09-21 18:55:10 -0400

    Delete PERMISSAO-EXCLUIR-MOTORISTAS.sql

--- PERMISSAO-EXCLUIR-MOTORISTAS.sql
+++
@@ -1,9 +0,0 @@
--- Permitir que usuários autenticados excluam registros de motoristas.
--- O botão de lixeira do sistema só aparece para registros com status ERRO.
-drop policy if exists "Usuários autenticados podem excluir motoristas" on public.motoristas;
-
-create policy "Usuários autenticados podem excluir motoristas"
-on public.motoristas
-for delete
-to authenticated
-using (true);
